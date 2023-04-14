665f4269f54e0896b8396b02bd0238f354380f78
EricCaparroso <126243096+EricCaparroso@users.noreply.github.com>
2023-04-14 11:56:50 +0100

    Getter y Setter

--- src/Personaje.java
+++ src/Personaje.java
@@ -8,5 +8,35 @@ public class Personaje {
         this.nombre = nombre;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPosicionX() {
+        return PosicionX;
+    }
+
+    public void setPosicionX(int posicionX) {
+        PosicionX = posicionX;
+    }
+
+    public int getPosicionY() {
+        return PosicionY;
+    }
+
+    public void setPosicionY(int posicionY) {
+        PosicionY = posicionY;
+    }
+
+    public int getVelocidad() {
+        return Velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        Velocidad = velocidad;
+    }
 }
